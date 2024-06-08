97e6c27a8e8bd8b14c4253b0c15bf867f348a9e7
Marco Antonio Evaristo <120408442+marcoaevaristo@users.noreply.github.com>
2024-06-08 19:37:38 -0300

    projeto inicial

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// scripts.js
+
+$(document).ready(function(){
+    // Smooth scroll para navegação
+    $('a.nav-link').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
